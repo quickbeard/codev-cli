878b69e0a99b77ab7b5e19f9782319d51729d445
minhn4 <nguyenm.cuny@gmail.com>
2026-04-20 08:55:27 +0700

    add logout command

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 import { render } from "ink";
 import { App } from "@/App.js";
+import { logout } from "@/auth.js";
+
+const command = process.argv[2];
+
+if (command === "logout") {
+	if (logout()) {
+		console.log("Logged out.");
+	} else {
+		console.log("Not logged in.");
+	}
+	process.exit(0);
+}
 
 render(<App />);
